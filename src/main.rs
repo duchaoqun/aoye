fc1e8102da02db772e1a5bec865153f1445bd5cb
duchaoqun <du.chao.qun@163.com>
2021-05-06 10:59:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("The value of x is: {}", x);
-}
-=======
-// 固定取值范围的类型
-
-// Unsigned Integer and Signed Integer
-
-// u8，取值范围 2 - (2^8-1)，通常表示字节序列，在文件I/O和网络I/O读取数据流的时候使用。
-
-// 动态取值范围的类型
-
-// 浮点数
 fn main() {
     let num = 42u32;
     let num: u32 = 42;
@@ -34,4 +17,3 @@ fn main() {
     println!("{:?}", std::f32::MIN);
     println!("{:?}", std::f32::MAX);
 }
->>>>>>> 944eac230b49f41fee56bbf512269b4d1c9e8ae6
